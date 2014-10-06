7787c19bd84b18142a0b4c675863347ffbd2c5ed
Tim Kellogg <tim@2lemetry.com>
2014-10-06 09:33:19 -0600

    upgrade to Rust 0.11 and rewrite
    The rewrite makes it more functional-styled. Prefer functions on plain data
    instead of method syntax. I'll be honest, method syntax doesn't work great
    in Rust, AFAICT

--- mqtt.rs
+++ mqtt.rs
@@ -1,30 +1,16 @@
-#![crate_id = "mqtt#0.10"]
+#![crate_name = "mqtt"]
 #![desc = "An Message Queue Telemetry Transport (MQTT) client"]
 #![crate_type = "lib"]
 #![feature(struct_variant)]
 
 
 pub mod mqtt {
-	use std::slice;
-	use std::slice::bytes::copy_memory;
-
-	trait MqttHeader {
-		fn asHeader(&self) -> u8;
-	}
 	
 	#[deriving(FromPrimitive)] 
 	pub enum QoS { 
 		AtMostOnce = 0, 
 		AtLeastOnce = 1, 
 		ExactlyOnce = 2
-	}
-
-	impl MqttHeader for QoS {
-
-		fn asHeader(&self) -> u8 {
-			let i = *self as u8;
-			i >> 5
-		}
 	}
 
 	#[deriving(FromPrimitive)] 
@@ -46,16 +32,15 @@ pub mod mqtt {
 		DISCONNECT = 14
 	}
 
-	impl MqttHeader for MessageType {
-		fn asHeader(&self) -> u8 {
-			*self as u8
-		}
+	pub struct LastWill {
+		topic: Box<str>,
+		msg: Box<str>,
+		qos: QoS,
+		retain: bool
 	}
 
-	#[inline]
-	fn parse_message_type(byte: u8) -> Option<MessageType> {
-		let masked = byte & 0xf0;
-		FromPrimitive::from_u8(masked)
+	pub enum Message {
+		Connack(u8)
 	}
 
 	fn parse_short(data: &[u8], index: uint) -> Option<u16> {
@@ -66,134 +51,88 @@ pub mod mqtt {
 		}))
 	}
 
-	pub struct LastWillOptions {
-		retain: bool,
-		qos: u8,
-		flag: bool,
-		topic: ~str,
-		msg: ~str
-	}
+	pub mod encode {
+		use mqtt::{LastWill};
 
-	pub struct ConnectOptions {
-		 clientId: ~str, 
-		 user: Option<~str>, 
-		 pw: Option<~str>, 
-		 will: Option<LastWillOptions>,
-		 clean: bool,
-		 keepAlive: u16
-	}
+		#[inline]
+		fn msb(i: u16) -> u8 {
+			((i & 0xff00) >> 8) as u8
+		}
 
-	pub enum Message {
-		Connect(ConnectOptions),
-		Connack ( u8 )
-	}
+		#[inline]
+		fn lsb(i: u16) -> u8 {
+			(i & 0xff) as u8
+		}
 
-	impl Message {
-		pub fn encode<T>(&self, result: |&[u8]| -> T) -> T {
-			#[inline]
-			fn opt_len(opt: &Option<~str>) -> uint {
-				match *opt { 
-					Some(ref s) => s.len() + 2,
-					None => 0
-				}
+		#[inline]
+		fn fshift<T>(opt: Option<&T>, by: uint) -> u8 {
+			opt.map_or(0, |_| 1 << by)
+		}
+
+		/// Encode connect message into a vector so it can be written into a TCP stream
+		pub fn connect(client_id: &str, user: Option<&str>, pass: Option<&str>, keep_alive: u16, clean: bool, lwt: Option<&LastWill>) -> Vec<u8> {
+			fn lwt_len(lwt: Option<&LastWill>) -> uint {
+				lwt.map_or(0, |opt| { opt.topic.len() + opt.msg.len() })
 			}
 
-			#[inline]
-			fn lwt_len(lwt: &Option<LastWillOptions>) -> uint {
-				match *lwt {
-					Some(ref opt) => opt.topic.len() + opt.msg.len(),
-					None => 0
-				}
+			fn lwt_flags(lwt: Option<&LastWill>) -> u8 {
+				lwt.map_or(0, |lw| { (lw.retain as u8) << 5 | ((lw.qos as u8) & 0x03) << 3 | 1 << 2 })
+			}
+			
+			let id_len = client_id.len() + 2;
+			let user_len = user.map_or(0, |u| u.len());
+			let pw_len = pass.map_or(0, |u| u.len());
+			// 10 = fixed_header + size + "MQTT" + protocol_lvl_byte + connect_flags + client_id+ will_topic + will_msg + will_flags
+			let msg_len = 10 + id_len + lwt_len(lwt) + user_len + pw_len;
+			
+			let flags = fshift(user.as_ref(), 7) |
+									fshift(pass.as_ref(), 6)   |
+									lwt_flags(lwt) |
+									(clean as u8) << 1;
+
+			let mut buf: Vec<u8> = Vec::with_capacity(msg_len);
+			buf.push_all([0x10, 10, 0x00, 0x04, 0x4c, 0x51, 0x54, 0x54, 4, flags, msb(keep_alive), lsb(keep_alive)]);
+
+			let client_len = client_id.len() as u16;
+			buf.push(msb(client_len));
+			buf.push(lsb(client_len));
+			buf.push_all(client_id.as_bytes());
+
+			for will in lwt.iter() {
+				let topic_len = will.topic.len() as u16;
+				buf.push(msb(topic_len));
+				buf.push(lsb(topic_len));
+				buf.push_all(will.topic.as_bytes());
+
+				let msg_len = will.msg.len() as u16;
+				buf.push(msb(msg_len));
+				buf.push(lsb(msg_len));
+				buf.push_all(will.msg.as_bytes());
 			}
 
-			fn lwt_flags(lwt: &Option<LastWillOptions>) -> u8 {
-				match *lwt {
-					Some(ref opt) => 
-						(opt.retain as u8) << 5 |
-						(opt.qos & 0x03) << 3   |
-						1 << 2,
-					None => 0
-				}
+			for user in user.iter() {
+				let len = user.len() as u16;
+				buf.push(msb(len));
+				buf.push(lsb(len));
+				buf.push_all(user.as_bytes());
 			}
 
-			#[inline]
-			fn msb(i: u16) -> u8 {
-				((i & 0xff00) >> 8) as u8
+			for pw in pass.iter() {
+				let len = pw.len() as u16;
+				buf.push(msb(len));
+				buf.push(lsb(len));
+				buf.push_all(pw.as_bytes());
 			}
 
-			#[inline]
-			fn lsb(i: u16) -> u8 {
-				(i & 0xff) as u8
-			}
-
-			#[inline]
-			fn fshift<T>(opt: &Option<T>, by: uint) -> u8 {
-				match opt.is_some() {
-					true => 1 << by,
-					false => 0
-				}
-			}
-
-			match *self {
-				Connect(ref opts) => {
-					let id_len = opts.clientId.len() + 2;
-					let lwt = lwt_len(&opts.will);
-					let user_len = opt_len(&opts.user);
-					let pw_len = opt_len(&opts.pw);
-					let payload_len = 10 + id_len + lwt + user_len + pw_len;
-					
-					let flags = fshift(&opts.user, 7) |
-											fshift(&opts.pw, 6)   |
-											lwt_flags(&opts.will) |
-											(opts.clean as u8) << 1;
-
-					let mut buf = slice::from_fn(payload_len + 12, |_| 0);
-					copy_memory(buf, [0x10, 10, 0x00, 0x04, 0x4c, 0x51, 0x54, 0x54, 4, flags, msb(opts.keepAlive), lsb(opts.keepAlive)]);
-
-					let client_len = opts.clientId.len() as u16;
-					copy_memory(buf.mut_slice_from(12), [msb(client_len), lsb(client_len)]);
-					copy_memory(buf.mut_slice_from(14), opts.clientId.as_bytes());
-
-					let mut end = 14 + client_len as uint;
-					for will in opts.will.iter() {
-						let topic_len = will.topic.len() as u16;
-						copy_memory(buf.mut_slice_from(end), [msb(topic_len), lsb(topic_len)]);
-						copy_memory(buf.mut_slice_from(end + 2), will.topic.as_bytes());
-						end += will.topic.len() + 2;
-
-						let msg_len = will.msg.len() as u16;
-						copy_memory(buf.mut_slice_from(end), [msb(msg_len), lsb(msg_len)]);
-						copy_memory(buf.mut_slice_from(end + 2), will.msg.as_bytes());
-						end += will.msg.len() + 2;
-					}
-
-					for user in opts.user.iter() {
-						let len = user.len() as u16;
-						copy_memory(buf.mut_slice_from(end), [msb(len), lsb(len)]);
-						copy_memory(buf.mut_slice_from(end + 2), user.as_bytes());
-						end += user.len() + 2;
-					}
-
-					for pw in opts.pw.iter() {
-						let len = pw.len() as u16;
-						copy_memory(buf.mut_slice_from(end), [msb(len), lsb(len)]);
-						copy_memory(buf.mut_slice_from(end + 2), pw.as_bytes());
-						end += pw.len() + 2;
-					}
-
-					result(buf)
-				}
-				Connack(_) => {
-					let s: ~[u8] = ~[0x20];
-					result(s)
-				}
-			}
+			buf
 		}
 	}
 
+	// 987326 Tuesday 8-10am
+
 	pub fn decode(data: &[u8]) -> Option<Message> {
 		let hd = data.head();
-		let msg = hd.and_then(|x| parse_message_type(*x));
+		let msg: Option<MessageType> = hd.and_then(|x| FromPrimitive::from_u8(*x));
 		msg.and_then(|x| match x {
 			CONNECT => None,
 			CONNACK => parse_connack(data),
@@ -223,26 +162,15 @@ pub mod mqtt {
 
 	#[cfg(test)]
 	pub mod tests {
-		use mqtt::{ConnectOptions, Connect};
 		use std::io::net::ip::SocketAddr;
 		use std::io::net::tcp::TcpStream;
 
 		#[test]
 		fn send_connect_msg() {
-			println!("parsing URI");
-			let addr = from_str::<SocketAddr>("127.0.0.1:1883").unwrap();
 			println!("connecting to localhost");
-			let mut socket = TcpStream::connect(addr).unwrap();
+			let mut socket = TcpStream::connect("127.0.0.1", 1883).unwrap();
 			println!("connected?!");
-			let opts = ConnectOptions {
-				clientId: ~"tim-rust",
-				user: None,
-				pw: None,
-				will: None,
-				clean: false,
-				keepAlive: 60
-			};
-			let connect = Connect(opts);
+			let connect_buf = mqtt::encode::connect("tim-rust");
 
 			let mut res = connect.encode(|v| socket.write(v));
 			res = res.and_then(|_| socket.flush());
